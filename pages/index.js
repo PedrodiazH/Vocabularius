c4dda2e263bb32e8b17f293d14dd1e9d271af909
PedrodiazH <133780341+PedrodiazH@users.noreply.github.com>
2023-05-18 22:16:10 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="PALABRAAA" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          hola
         </p>
       </main>
 
